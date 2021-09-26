dee908efee7ffd338e2ee41878f853155bf8ffdd
rajeev <rajeev.goonie98@gmail.com>
2021-09-26 01:57:45 -0700

    added image picker functionality

--- App.js
+++ App.js
@@ -2,8 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import logo from './assets/logo.png'
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePictureAsync = async () => {
+    let permissonResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if ( permissonResult.granted === false ){
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+    
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if( pickerResult.cancelled === true ) {
+      return;
+    }
+
+    setSelectedImage({localUri: pickerResult.uri})
+  };
+
+  if(selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  }
+
+
   return (
     <View style={styles.container}>
       <Image source={{uri: "https://i.imgur.com/TkIrScD.png"}} style={styles.logo}/>
@@ -11,8 +43,7 @@ export default function App() {
         To share a photo from your phone with a friend, just press the button below!
       </Text>
 
-      <TouchableOpacity
-        onPress={() => alert("Hello World!")} style={styles.button}>
+      <TouchableOpacity onPress={openImagePictureAsync} style={styles.button}>
         <Text style={styles.buttonText}>Pick a Photo</Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
@@ -45,5 +76,10 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 20,
     color: "#fff",
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
   },
 });
